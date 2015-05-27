32a52f143a1762ca1fd2486c6e46988eb1833012
Erich Oliveira <ericholiveira10@gmail.com>
2015-05-27 11:26:40 -0300

    timer for drivers

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var callCb = function(error,result,cb,initialTime,sender, receiver, message, headers){
+var callCbActor = function(error,result,cb,initialTime,sender, receiver, message, headers){
   var totalTime = new Date().getTime() - initialTime;
   setImmediate(function(){
     cb(error,{
@@ -13,32 +13,65 @@ var callCb = function(error,result,cb,initialTime,sender, receiver, message, hea
     });
   });
 };
-module.exports = function(cb,filter){
-  return function(opt){
-    opt.listenTo.onCreateActor(function(actor){
+var onCreateActorListener = function(cb,filter){
+  filter = filter || function(){return true;};
+  return function(actor){
+    if(cb && filter(actor)){
       var _process = actor.process.bind(actor);
       actor.process = function(body,headers,sender,receiver){
-        if(!filter || filter({sender:sender,receiver:receiver,body:body,headers:headers})){
-          var initialTime = new Date().getTime();
-          var res = _process(body,headers,sender,receiver);
-          if(res && res.constructor.name==='Promise'){
-            return res.then(function(result){
-              callCb(null,result,cb,initialTime,sender, receiver, body, headers);
-              return result;
-            }).catch(function(err){
-              callCb(err,null,cb,initialTime,sender, receiver, body, headers);
-              throw err;
-            });
-          }else{
-            callCb(null,res,cb,initialTime,sender, receiver, body, headers);
-            return res;
-          }
+        var initialTime = new Date().getTime();
+        var res = _process(body,headers,sender,receiver);
+        if(res && res.constructor.name==='Promise'){
+          return res.then(function(result){
+            callCbActor(null,result,cb,initialTime,sender, receiver, body, headers);
+            return result;
+          }).catch(function(err){
+            callCbActor(err,null,cb,initialTime,sender, receiver, body, headers);
+            throw err;
+          });
         }else{
-          return _process(body,headers,sender,receiver);
+          callCbActor(null,res,cb,initialTime,sender, receiver, body, headers);
+          return res;
         }
       };
       actor.unsubscribe();
       actor.unsubscribe = actor.stream.onValue(actor._doProcess);
+    }
+  }
+};
+var callCbDriver = function(error,result,cb,initialTime,driver){
+  var totalTime = new Date().getTime() - initialTime;
+  setImmediate(function(){
+    cb(error,{
+      time:totalTime,
+      driver:driver,
+      result: result
     });
+  });
+};
+var onCreateDriverListener = function(cb,filter){
+  filter = filter || function(){return true;};
+  return function(driver){
+    if(cb && filter(driver)){
+      var _send = driver.send.bind(driver);
+      driver.send = function(){
+        var initialTime = new Date().getTime();
+        return _send.apply(this,Array.prototype.slice.call(arguments)).then(function(result){
+          callCbDriver(null,result,cb,initialTime,driver);
+          return result;
+        }).catch(function(err){
+          callCbDriver(err,null,cb,initialTime,driver);
+          throw err;
+        });
+      };
+    }
+  };
+};
+module.exports = function(userOpt){
+  return function(opt){
+    var actor = userOpt.actor || {};
+    var driver = userOpt.driver || {};
+    opt.listenTo.onCreateActor(onCreateActorListener(actor.callback,actor.filter));
+    opt.listenTo.onCreateDriver(onCreateDriverListener(driver.callback,driver.filter));
   };
 };
